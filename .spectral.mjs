d3d2d6a60b73270dc71be48468c883282755dd8b
reddyprasaddakshanapu <85872848+reddyprasaddakshanapu@users.noreply.github.com>
2023-08-30 14:04:07 +0530

    Delete .spectral.mjs
    delete file

--- .spectral.mjs
+++
@@ -1,2 +0,0 @@
-import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwNjIxMg/spectral.js?branch_slug=main&token=4b5cd62a-ee2c-4cd9-9292-662eadeadfd3";
-export default { extends: ruleset };
